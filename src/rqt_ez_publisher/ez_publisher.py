5bbb71d89d45129a3cdbe15aff21ccc8e1b33335
Takashi Ogura <t.ogura@gmail.com>
2014-06-09 00:27:22 +0900

    Prototype of EzPublisher

---
+++ src/rqt_ez_publisher/ez_publisher.py
@@ -0,0 +1,43 @@
+import os
+import rospy
+from rqt_ez_publisher.ez_publisher_widget import EasyPublisherWidget
+from qt_gui.plugin import Plugin
+
+class EzPublisherPlugin(Plugin):
+
+    def __init__(self, context):
+        super(EzPublisherPlugin, self).__init__(context)
+        self.setObjectName('EzPublisher')
+        from argparse import ArgumentParser
+        parser = ArgumentParser()
+        parser.add_argument("-q", "--quiet", action="store_true",
+                      dest="quiet",
+                      help="Put plugin in silent mode")
+        args, unknowns = parser.parse_known_args(context.argv())
+        # Create QWidget
+        self._widget = EasyPublisherWidget()
+        self._widget.setObjectName('EzPublisherPluginUi')
+        if context.serial_number() > 1:
+            self._widget.setWindowTitle(self._widget.windowTitle() + (' (%d)' % context.serial_number()))
+        context.add_widget(self._widget)
+
+    def shutdown_plugin(self):
+        pass
+        #self._widget.shutdown()
+
+    def save_settings(self, plugin_settings, instance_settings):
+        rospy.loginfo('saved %s'%self._widget.get_texts())
+        instance_settings.set_value('texts', self._widget.get_texts())
+
+
+    def restore_settings(self, plugin_settings, instance_settings):
+        texts = instance_settings.value('texts')
+        if texts:
+            for text in texts:
+                self._widget.add_slider_by_text(text)
+
+    #def trigger_configuration(self):
+        # Comment in to signal that the plugin has a way to configure
+        # This will enable a setting button (gear icon) in each dock widget title bar
+        # Usually used to open a modal configuration dialog
+
---
+++ src/rqt_ez_publisher/ez_publisher_widget.py
@@ -0,0 +1,231 @@
+import rospy
+import roslib.message
+import sys
+
+from python_qt_binding import QtGui
+from python_qt_binding.QtCore import Qt
+
+
+def find_topic_name(full_text, topic_dict):
+    if full_text[0] != '/':
+        full_text = '/' + full_text
+    splited_text = full_text.split('/')[1:]
+    topic_name = ''
+    while not topic_name in topic_dict and splited_text:
+        topic_name += '/' + splited_text[0]
+        splited_text = splited_text[1:]
+    if splited_text:
+        return (topic_name, splited_text)
+    else:
+        return (None, None)
+
+        
+class ValueWidget(QtGui.QWidget):
+    def __init__(self, topic_name, attributes, message, publisher, parent=None):
+        QtGui.QWidget.__init__(self, parent=parent)
+        self._attributes = attributes
+        self._publisher = publisher
+        self._message = message
+        self.setup_ui(topic_name + '/' + '/'.join(attributes))
+
+    def publish_value(self, value):
+        message_target = self._message
+        for attr in self._attributes[:-1]:
+            message_target = message_target.__getattribute__(attr)
+        message_target.__setattr__(self._attributes[-1], value)
+        self._publisher.publish(self._message)
+
+    def setup_ui(self, name):
+        pass
+
+
+class BoolValueWidget(ValueWidget):
+    def __init__(self, topic_name, attributes, message, publisher, parent=None):
+        ValueWidget.__init__(self, topic_name, attributes, message, publisher, parent=parent)
+
+    def state_changed(self, state):
+        self.publish_value(self._check_box.isChecked())
+
+    def setup_ui(self, name):
+        self._horizontal_layout = QtGui.QHBoxLayout()
+        topic_label = QtGui.QLabel(name)
+        self._check_box = QtGui.QCheckBox()
+        self._check_box.stateChanged.connect(self.state_changed)
+        self._horizontal_layout.addWidget(topic_label)
+        self._horizontal_layout.addWidget(self._check_box)
+        self.setLayout(self._horizontal_layout)
+
+class StringValueWidget(ValueWidget):
+    def __init__(self, topic_name, attributes, message, publisher, parent=None):
+        ValueWidget.__init__(self, topic_name, attributes, message, publisher, parent=parent)
+
+    def input_text(self):
+        self.publish_value(str(self._line_edit.text()))
+
+    def setup_ui(self, name):
+        self._horizontal_layout = QtGui.QHBoxLayout()
+        topic_label = QtGui.QLabel(name)
+        self._line_edit = QtGui.QLineEdit()
+        self._line_edit.returnPressed.connect(self.input_text)
+        self._horizontal_layout.addWidget(topic_label)
+        self._horizontal_layout.addWidget(self._line_edit)
+        self.setLayout(self._horizontal_layout)
+
+
+class IntValueWidget(ValueWidget):
+    def __init__(self, topic_name, attributes, message, publisher, parent=None):
+        ValueWidget.__init__(self, topic_name, attributes, message, publisher, parent=parent)
+    
+    def slider_changed(self, value):
+        self._lcd.display(value)
+        self.publish_value(value)
+
+    def setup_ui(self, name):
+        self._horizontal_layout = QtGui.QHBoxLayout()
+        topic_label = QtGui.QLabel(name)
+        self._min_spin_box = QtGui.QSpinBox()
+        self._min_spin_box.setMaximum(10000)
+        self._min_spin_box.setMinimum(-10000)
+        self._slider = QtGui.QSlider(Qt.Horizontal)
+        self._slider.setTickPosition(QtGui.QSlider.TicksBelow)
+        self._slider.valueChanged.connect(self.slider_changed)
+        self._max_spin_box = QtGui.QSpinBox()
+        self._max_spin_box.setMaximum(10000)
+        self._max_spin_box.setMinimum(-10000)
+        self._lcd = QtGui.QLCDNumber()
+        self._min_spin_box.valueChanged.connect(self._slider.setMinimum)
+        self._max_spin_box.valueChanged.connect(self._slider.setMaximum)
+        self._min_spin_box.setValue(-10)
+        self._max_spin_box.setValue(10)
+        self._slider.setValue(0)
+        self._horizontal_layout.addWidget(topic_label)
+        self._horizontal_layout.addWidget(self._min_spin_box)
+        self._horizontal_layout.addWidget(self._slider)
+        self._horizontal_layout.addWidget(self._max_spin_box)
+        self._horizontal_layout.addWidget(self._lcd)
+        self.setLayout(self._horizontal_layout)
+
+
+class DoubleValueWidget(ValueWidget):
+    def __init__(self, topic_name, attributes, message, publisher, parent=None):
+        ValueWidget.__init__(self, topic_name, attributes, message, publisher, parent=parent)
+
+    def slider_changed(self, val):
+        value = self._min_spin_box.value() + (self._max_spin_box.value() - self._min_spin_box.value()) / 100.0 * val
+        self._lcd.display(value)
+        self.publish_value(value)
+    
+    def setup_ui(self, name):
+        self._horizontal_layout = QtGui.QHBoxLayout()
+        topic_label = QtGui.QLabel(name)
+        self._min_spin_box = QtGui.QDoubleSpinBox()
+        self._min_spin_box.setMaximum(10000)
+        self._min_spin_box.setMinimum(-10000)
+        self._min_spin_box.setValue(-1.0)
+        self._slider = QtGui.QSlider(Qt.Horizontal)
+        self._slider.setTickPosition(QtGui.QSlider.TicksBelow)
+        self._slider.valueChanged.connect(self.slider_changed)
+        self._max_spin_box = QtGui.QDoubleSpinBox()
+        self._max_spin_box.setMaximum(10000)
+        self._max_spin_box.setMinimum(-10000)
+        self._max_spin_box.setValue(1.0)
+        self._lcd = QtGui.QLCDNumber()
+        self._slider.setValue(50)
+        self._horizontal_layout.addWidget(topic_label)
+        self._horizontal_layout.addWidget(self._min_spin_box)
+        self._horizontal_layout.addWidget(self._slider)
+        self._horizontal_layout.addWidget(self._max_spin_box)
+        self._horizontal_layout.addWidget(self._lcd)
+        self.setLayout(self._horizontal_layout)
+
+
+class EasyPublisherWidget(QtGui.QWidget):
+    def __init__(self, parent=None):
+        QtGui.QWidget.__init__(self, parent=parent)
+        self.setup_ui()
+        # model
+        self._publishers = {}
+        self._messages = {}
+        self._texts = []
+        
+    def add_slider(self):
+        self.add_slider_by_text(str(self._line_edit.text()))
+
+    def add_widget(self, output_type, topic_name, attributes):
+        if output_type == float:
+            self._main_vertical_layout.addWidget(
+                DoubleValueWidget(topic_name, attributes, self._messages[topic_name], self._publishers[topic_name]))
+            return True
+        elif output_type == int:
+            self._main_vertical_layout.addWidget(
+                IntValueWidget(topic_name, attributes, self._messages[topic_name], self._publishers[topic_name]))
+            return True
+        elif output_type == bool:
+            self._main_vertical_layout.addWidget(
+                BoolValueWidget(topic_name, attributes, self._messages[topic_name], self._publishers[topic_name]))
+            return True
+        elif output_type == str:
+            self._main_vertical_layout.addWidget(
+                StringValueWidget(topic_name, attributes, self._messages[topic_name], self._publishers[topic_name]))
+            return True
+        else:
+            rospy.logerr('not supported type %s'%type(message_target))
+        return False
+        
+    def add_slider_by_text(self, text):
+        _, _, topic_types = rospy.get_master().getTopicTypes()
+        topic_dict = dict(topic_types)
+        topic_name, attributes = find_topic_name(text, topic_dict)
+        if not topic_name:
+            rospy.logerr('%s not found'%text)
+            return
+        topic_type_str = topic_dict[topic_name]
+        message_class = roslib.message.get_message_class(topic_type_str)
+        if not topic_name in self._publishers:
+            self._publishers[topic_name] = rospy.Publisher(topic_name, message_class)
+        if not topic_name in self._messages:
+            self._messages[topic_name] = message_class()
+        message_target = self._messages[topic_name]
+        try:
+            for attr in attributes:
+                message_target = message_target.__getattribute__(attr)
+        except AttributeError, e:
+            rospy.logerr(e)
+            return
+        if self.add_widget(type(message_target), topic_name, attributes):
+            self._texts.append(text)
+
+    def get_texts(self):
+        return self._texts
+
+    def setup_ui(self):
+        horizontal_layout = QtGui.QHBoxLayout()
+        topic_label = QtGui.QLabel('topic/data (rqt_plot like)')
+        horizontal_layout.addWidget(topic_label)
+        self._line_edit = QtGui.QLineEdit()
+        self._line_edit.returnPressed.connect(self.add_slider)
+        horizontal_layout.addWidget(self._line_edit)
+        add_button = QtGui.QPushButton('+')
+        horizontal_layout.addWidget(add_button)
+        add_button.clicked.connect(self.add_slider)
+        self._main_vertical_layout = QtGui.QVBoxLayout()
+        self._main_vertical_layout.addLayout(horizontal_layout)
+        self._main_vertical_layout.setAlignment(horizontal_layout, Qt.AlignTop)
+        self.setLayout(self._main_vertical_layout)
+
+    def shutdown(self):
+        for pub in self._publishers.values():
+            pub.unregister()
+
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    main_window = QtGui.QMainWindow()
+    main_widget = EasyPublisherWidget()
+    main_window.setCentralWidget(main_widget)
+    main_window.show()
+    app.exec_()
+
+if __name__ == '__main__':
+    rospy.init_node('ez_publisher')
+    main()
