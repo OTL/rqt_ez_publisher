9ce2312f40396b4c340ebac932717a74738fc5e2
Takashi Ogura <t.ogura@gmail.com>
2014-07-19 19:19:02 +0900

    Fix setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['rqt_ez_publisher'],
+    packages=['rqt_ez_publisher', 'rqt_ez_publisher.widget',
+              'rqt_ez_publisher.publisher'],
     package_dir={'': 'src'},
 )
 
